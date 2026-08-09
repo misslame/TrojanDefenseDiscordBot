382f33dc571dfd0b7804878116edb1c26c7c42e4
misslame <14coxkm@gmail.com>
2026-08-09 13:02:07 -0700

    Add temp command code, remove unnecessary command

---
+++ commands/moderation/ban.py
@@ -0,0 +1,19 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class BanUser(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="ban", description="Check latency")
+    async def ban(self, interaction: discord.Interaction):
+        # Use interaction.response to reply to slash commands
+        latency = round(self.bot.latency * 1000)
+        await interaction.response.send_message(f"🏓 Pong! {latency}ms.")
+
+
+# The setup function that main.py calls to register the Command
+async def setup(bot: commands.Bot):
+    await bot.add_cog(BanUser(bot))
